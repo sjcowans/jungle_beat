645b9552daf3e0508fe07afeb5102a81041b0c0e
Sean Cowans <zedesia@yahoo.com>
2023-02-06 13:58:17 -0700

    Add find test to linkedlist_spec

--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -99,4 +99,16 @@ RSpec.describe LinkedList do
 
     expect(list.to_string).to eq("dop woo plop suu")
   end
+
+  it 'has working find method' do
+    list = LinkedList.new
+    list.append("deep")
+    list.append("woo")
+    list.append("shi")
+    list.append("shu")
+    list.append("blop")
+
+    expect(list.find(2, 1)).to eq("shi")
+    expect(list.find(1, 3)).to eq("wuu shi shu")
+  end
 end
